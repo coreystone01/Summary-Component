5b5d1561e785181e1e698067b673cf7d32698950
Corey <corey.stone001@gmail.com>
2023-09-19 09:32:11 -0700

    seperate out the scores tags into a scores partial as we are going to use js to change their values; add script.js to project

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const summaryConsole = {
+
+resultsScore      : 0,
+resultsPercentage : 0,
+reactionScore     : 0,
+memoryScore       : 0,
+verbalScore       : 0,
+visualScore       : 0,    
+submitButtonState       : "unpressed",
+
+    toggleButton: function(buttonStatus){
+        this.submitButtonStatebuttonState = buttonStatus;
+    }
+}
