81005465d4c7750ecd0f5f6fd1a73413d7f1e2b1
Corey <corey.stone001@gmail.com>
2023-09-19 15:14:50 -0700

    fix formatting in script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-
-// const summaryConsole = {
-
-// resultsScore      : 0,
-// resultsPercentage : 0,
-// reactionScore     : 0,
-// memoryScore       : 0,
-// verbalScore       : 0,
-// visualScore       : 0,    
-// submitButtonState : "unpressed",
-
-//     toggleButton: function(buttonStatus){
-//         this.submitButtonStatebuttonState = buttonStatus;
-//     }
-// }
-
 import Score from "./score";
 
 function updateSummaryValues(){
