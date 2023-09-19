9be31ed6a71597f4712e76dcbd15d458c2be3ed1
Corey <corey.stone001@gmail.com>
2023-09-19 15:14:13 -0700

    create an updateSummaryValues() function using our Score class that will generate values for the reaction, memory, verbal and visual tagline elements

--- script.js
+++ script.js
@@ -16,5 +16,24 @@
 
 import Score from "./score";
 
-const getReactionScore = new Score();
-getReactionScore.Score.reactionScore;
+function updateSummaryValues(){
+    const getScores = new Score();
+    // grab all the summary__container__content from HTML
+    const scoreContainers = document.querySelectorAll('.summary__container__content');
+
+    // run through each summary__container__content container
+    scoreContainers.forEach(container => {
+        // call our divs and their name attributes
+        const divName = container.getAttribute('name');
+        // call the generateRandomScore method from Score() class
+        const scoreValue = getScores.generateRandomScore();
+
+        // make a call to our summary__container_content classes and we're going to update their HTML content
+        const elementValue = container.querySelector('.summary__container__content')
+        elementValue.textContent = '${scoreValue}';
+    });
+}
+
+// Now we add an event listener to our continue button that will run our updateSummaryValues function
+const contineButton = document.querySelector('button');
+contineButton.addEventListener('click', updateSummaryValues);
