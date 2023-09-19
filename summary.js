84a6426566c752a3ca3550b015e1d797d692cfdc
Corey <corey.stone001@gmail.com>
2023-09-19 14:15:38 -0700

    create a summary class that will calculate the overall score that will generate in the summary section of our HTML; also generate a random score for each of our performance metrics and use these metrics to determine the overall score. 465 participants used this dashboard so we can use this number to determine how well the participant did against others

---
+++ summary.js
@@ -0,0 +1,37 @@
+class Summary {
+    constructor(
+        // params
+        percentage,
+        reactionScore,
+        memoryScore,
+        verbalScore,
+        visualScore
+    )
+    {
+        // define properties
+        
+        this.reactionScore = this.generateRandomScore();
+        this.memoryScore = this.generateRandomScore();
+        this.verbalScore = this.generateRandomScore();
+        this.visualScore = this.generateRandomScore();
+        this.percentage = this.calculateOverallScore();
+    }
+
+    // define methods
+    generateRandomScore(){
+        return Math.floor(Math.random() * 101);
+    }
+
+    calculatePerformancePercentage (){
+        const performance = this.reactionScore + this.memoryScore + this.verbalScore + this.visualScore;
+        return (performance / 465) * 100;
+    }
+
+    calculateOverallScore () {
+        const score = this.reactionScore + this.memoryScore + this.verbalScore + this.visualScore;
+        return (score / 400);
+    }
+    
+}
+
+const summary = new Summary();
